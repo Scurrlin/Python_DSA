133aa092f0a12f02f75f368120a9cc25933b01dd
Sean Currlin <sean.currlin@gmail.com>
2023-11-27 22:08:48 -0500

    update trees.py

--- Trees/Trees.py
+++ Trees/Trees.py
@@ -2,6 +2,6 @@
 
 # A tree is considered "perfect" if all nodes are completely full across each tier of the tree
 
-# A is "complete" if nodes are full from left to right across each tier of the tree. The final tier of the tree can continue to remain "complete" even if the tree isn't "perfect". The most important criteria for "completeness" to remember is that it is measured from left to right.
+# A tree is considered "complete" if nodes are full from left to right across each tier of the tree. The final tier of the tree can continue to remain "complete" even if the tree isn't "perfect". The most important criteria for "completeness" to remember is that it is measured from left to right.
 
 # Top node is a "parent" node, the nodes the "parent" node is pointing to is called a "child" node. "Child" nodes that share a "parent" node are called "siblings". A "child" node that don't point to another node is called a "leaf".
