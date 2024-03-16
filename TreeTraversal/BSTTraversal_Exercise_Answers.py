acee541c5dc6a25e673e5cfbf450433246d84aea
Sean Currlin <sean.currlin@gmail.com>
2024-03-16 13:52:31 -0400

    BSTTraversal reformat

--- TreeTraversal/BSTTraversal_Exercise_Answers.py
+++ TreeTraversal/BSTTraversal_Exercise_Answers.py
@@ -16,35 +16,49 @@ def BFS(self):
     return results
 
 def BFS(self):
-    # set current_node to the root of the tree
+    
+# set current_node to the root of the tree
+    
     current_node = self.root
     
-    # create an empty queue to store nodes to visit
+# create an empty queue to store nodes to visit
+
     queue = []
     
-    # create an empty list to store the values of visited nodes
+# create an empty list to store the values of visited nodes
+
     results = []
     
-    # add the root node to the queue
+# add the root node to the queue
+    
     queue.append(current_node)
  
-    # continue until all nodes have been visited
+# continue until all nodes have been visited
+    
     while len(queue) > 0:
-        # remove the first node from the queue
+       
+# remove the first node from the queue
+        
         current_node = queue.pop(0)
         
-        # add the value of the visited node to the results list
+# add the value of the visited node to the results list
+
         results.append(current_node.value)
         
-        # if the visited node has a left child, add it to the queue
+
+# if the visited node has a left child, add it to the queue
+
         if current_node.left is not None:
             queue.append(current_node.left)
         
-        # if the visited node has a right child, add it to the queue
+
+# if the visited node has a right child, add it to the queue
+        
         if current_node.right is not None:
             queue.append(current_node.right)
  
-    # return the list of visited node values
+# return the list of visited node values
+    
     return results
 
 # DFS: PreOrder
@@ -61,24 +75,32 @@ def dfs_pre_order(self):
     return results
 
 def dfs_pre_order(self):
-    results = []  # create an empty list to store the values of visited nodes
+    results = [] 
+    
+# create an empty list to store the values of visited nodes
  
     def traverse(current_node):
-        # append the value of the current node to the results list
+        
+# append the value of the current node to the results list
+
         results.append(current_node.value)
  
-        # if the current node has a left child, recursively traverse it
+# if the current node has a left child, recursively traverse it
+
         if current_node.left is not None:
             traverse(current_node.left)
  
-        # if the current node has a right child, recursively traverse it
+# if the current node has a right child, recursively traverse it
+
         if current_node.right is not None:
             traverse(current_node.right)
  
-    # start the pre-order traversal from the root of the tree
+# start the pre-order traversal from the root of the tree
+
     traverse(self.root)
  
-    # return the list of visited node values
+# return the list of visited node values
+
     return results
 
 # DFS: PostOrder
@@ -96,24 +118,32 @@ def dfs_post_order(self):
 
 
 def dfs_post_order(self):
-    results = []  # create an empty list to store the values of visited nodes
+    results = []
+
+# create an empty list to store the values of visited nodes
  
     def traverse(current_node):
-        # if the current node has a left child, recursively traverse it
+        
+# if the current node has a left child, recursively traverse it
+
         if current_node.left is not None:
             traverse(current_node.left)
  
-        # if the current node has a right child, recursively traverse it
+# if the current node has a right child, recursively traverse it
+
         if current_node.right is not None:
             traverse(current_node.right)
  
-        # append the value of the current node to the results list
+# append the value of the current node to the results list
+
         results.append(current_node.value)
  
-    # start the post-order traversal from the root of the tree
+# start the post-order traversal from the root of the tree
+
     traverse(self.root)
  
-    # return the list of visited node values
+# return the list of visited node values
+
     return results
 
 # DFS: InOrder
@@ -130,22 +160,30 @@ def dfs_in_order(self):
     return results
 
 def dfs_in_order(self):
-    results = []  # create an empty list to store the values of visited nodes
+    results = []
+
+# create an empty list to store the values of visited nodes
  
     def traverse(current_node):
-        # if the current node has a left child, recursively traverse it
+        
+# if the current node has a left child, recursively traverse it
+
         if current_node.left is not None:
             traverse(current_node.left)
  
-        # append the value of the current node to the results list
+# append the value of the current node to the results list
+
         results.append(current_node.value)
  
-        # if the current node has a right child, recursively traverse it
+# if the current node has a right child, recursively traverse it
+
         if current_node.right is not None:
             traverse(current_node.right)
  
-    # start the in-order traversal from the root of the tree
+# start the in-order traversal from the root of the tree
+
     traverse(self.root)
  
-    # return the list of visited node values
+# return the list of visited node values
+
     return results
--- TreeTraversal/BSTTraversal_IQ+Answers.py
+++ TreeTraversal/BSTTraversal_IQ+Answers.py
@@ -8,17 +8,27 @@ def is_valid_bst(self):
     return True
 
 def is_valid_bst(self):
-    # Get node values of the binary search tree in ascending order
+
+# Get node values of the binary search tree in ascending order
+    
     node_values = self.dfs_in_order()
-    # Iterate through the node values using a for loop
+    
+# Iterate through the node values using a for loop
+    
     for i in range(1, len(node_values)):
-        # Check if each node value is greater than the previous node value
+        
+# Check if each node value is greater than the previous node value
+        
         if node_values[i] <= node_values[i-1]:
-            # If node values are not sorted in ascending order, the binary
-            # search tree is not valid, so return False
+            
+# If node values are not sorted in ascending order, the binary
+# search tree is not valid, so return False
+
             return False
-    # If all node values are sorted in ascending order, the binary search tree
-    # is a valid binary search tree, so return True
+
+# If all node values are sorted in ascending order, the binary search tree
+# is a valid binary search tree, so return True
+    
     return True
 
 # BST: Kth Smallest Node
@@ -42,29 +52,42 @@ def kth_smallest(self, k):
     return None
 
 def kth_smallest(self, k):
-        # create a stack to hold nodes
+        
+# create a stack to hold nodes
+
         stack = []    
-        # start at the root of the tree      
+        
+# start at the root of the tree      
+
         temp = self.root    
         
         while stack or temp:
-            # traverse to the leftmost node
+            
+# traverse to the leftmost node
+            
             while temp: 
-                # add the node to the stack                
+                
+# add the node to the stack                
+                
                 stack.append(temp)      
                 temp = temp.left
             
-            # pop the last node added to the stack
+# pop the last node added to the stack
+            
             temp = stack.pop()           
             k -= 1
-            # if kth smallest element is found, return the value
+            
+# if kth smallest element is found, return the value
+            
             if k == 0:                  
                 return temp.value
             
-            # move to the right child of the node
+# move to the right child of the node
+            
             temp = temp.right           
             
-        # if k is greater than the number of nodes in the tree, return None
+# if k is greater than the number of nodes in the tree, return None
+
         return None
 
 # BST: Kth Smallest Node (Recursive Solution)
@@ -92,33 +115,48 @@ def kth_smallest_helper(self, node, k):
     return None
 
 def kth_smallest(self, k):
-        # initialize the number of nodes visited to 0
+
+# initialize the number of nodes visited to 0
+        
         self.kth_smallest_count = 0
-        # call the helper function with the root node and k
+
+# call the helper function with the root node and k
+        
         return self.kth_smallest_helper(self.root, k)
  
 def kth_smallest_helper(self, node, k):
     if node is None:
-        # if the current node is None, return None
+        
+# if the current node is None, return None
+
         return None
  
-    # recursively call the helper function on the left child of the node and store the result in left_result
+# recursively call the helper function on the left child of the node and store the result in left_result
+    
     left_result = self.kth_smallest_helper(node.left, k)
     if left_result is not None:
-        # if left_result is not None, return it
+        
+# if left_result is not None, return it
+        
         return left_result
  
-    # increment the number of nodes visited by 1
+# increment the number of nodes visited by 1
+    
     self.kth_smallest_count += 1
     if self.kth_smallest_count == k:
-        # if the kth smallest element is found, return the value of the current node
+        
+# if the kth smallest element is found, return the value of the current node
         return node.value
  
-    # recursively call the helper function on the right child of the node and store the result in right_result
+# recursively call the helper function on the right child of the node and store the result in right_result
+    
     right_result = self.kth_smallest_helper(node.right, k)
     if right_result is not None:
-        # if right_result is not None, return it
+
+# if right_result is not None, return it
+        
         return right_result
  
-    # if the kth smallest element is not found, return None
+# if the kth smallest element is not found, return None
+    
     return None
